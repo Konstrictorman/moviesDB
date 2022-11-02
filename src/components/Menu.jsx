b392a44f3040def59a08528dccb0e6f769edba23
Konstrictorman <rikhardho@hotmail.com>
2022-11-01 20:52:47 -0500

    ComboBox included

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import ComboBox from './ComboBox';
 import MenuItem from './MenuItem';
 
 export const Menu = () => {
+
+   const options = useMemo(() => {
+      const arr = [
+         {
+            id:1,
+            label:'RELEASE DATE',
+         },
+         {
+            id:2,
+            label:'ALPHABETICALLY',
+         }
+      ]
+      return arr;
+   }, [])
+
   return (
     <div className="menu_container">
 
@@ -11,7 +27,9 @@ export const Menu = () => {
       <MenuItem label="HORROR"/>
       <MenuItem label="CRIME"/>
 
-      <div className='menu_item menu_right'>SORT BY</div>
+      <div className='menu_right menu_combo'>
+         <ComboBox label="SORT BY" options={options}/>
+      </div>
     </div>
   );
 };
